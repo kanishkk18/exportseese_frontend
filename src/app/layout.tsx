9d043bb82efa4e15abdad2e217074a8851310254
kanishkk Bansal <168306760+kanishkk18@users.noreply.github.com>
2025-04-01 14:35:05 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-         {/* <SecurityFeatures /> */}
+         <SecurityFeatures />
          <ThemeProvider
             attribute="class"
             defaultTheme="system"
